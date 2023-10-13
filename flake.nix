2dddd9710f458453d5a314bbb061159f857c3128
BlueKossa <dexter@stormdal.se>
2023-10-14 01:27:09 +0200

    Added personal tmux, may remove

--- flake.nix
+++ flake.nix
@@ -20,6 +20,26 @@
             pkgs.ncurses
             pkgs.llvmPackages_14.llvm.dev
           ];
+          # Idk
+          shellHook = ''
+            session="eris-dev"
+            sessionExists=$(tmux list-sessions | grep $session)
+
+            if [ "$sessionExists" != "" ]; then
+              tmux kill-session -t $session
+            fi
+
+            tmux new-session -d -s $session
+            tmux set -g mouse on
+            tmux set -g mouse-select-window on
+
+            tmux send-keys -t $session:1 "nvim src/main.rs" C-m
+            tmux rename-window -t $session:1 "nvim"
+            
+            tmux new-window -t $session -n "shell"
+            tmux attach-session -t $session:1
+            exit
+          '';
 
           LLVM_SYS_140_PREFIX = pkgs.llvmPackages_14.llvm.dev;
         };
